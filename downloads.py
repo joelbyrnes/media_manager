45d7a7c6fa7cf499743e5c207c3a32dcb843541e
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-07-25 13:38:50 +1000

    fix rars bug

--- downloads.py
+++ downloads.py
@@ -10,11 +10,12 @@ out_dir = '/home/joel/mac-mini.joel/Media Downloads/out_tv'
 def list_has_rars(files):
 	for f in files:
 		if f.lower().endswith(".rar") or f.lower().endswith(".r00"):
-			print "found rar-like file %s" % f
+			print "(found rar-like file %s)" % f
 			return True
 	return False
 
 def dir_has_rars(path):
+	if not os.path.exists(path): raise Exception("path does not exist: " + path)
 	if not os.path.isdir(path): return False
 
 	for root, subdirs, files in os.walk(path):
@@ -22,6 +23,10 @@ def dir_has_rars(path):
 			return True
 	return False
 
+def move(names, cwd, out_dir):
+	for f in names:
+		print "moving %s to %s/%s" % (f, out_dir, f)
+		os.rename("%s/%s" % (cwd, f), "%s/%s" % (out_dir, f))
 
 def print_possibles(possibles, tnames):
 	for p in possibles:
@@ -31,7 +36,7 @@ def print_possibles(possibles, tnames):
 		else:
 			print "** NOT in Tx: " + p
 
-			if dir_has_rars(p):
+			if dir_has_rars(complete_dir + "/" + p):
 				print "  ** HAS RARS"
 
 
@@ -52,15 +57,10 @@ def main(complete_dir, out_dir):
 
 	print_possibles(mkvs, tnames)
 
-	def move(names, cwd, out_dir):
-		for f in names:
-			print "moving %s to %s/%s" % (f, out_dir, f)
-			os.rename("%s/%s" % (cwd, f), "%s/%s" % (out_dir, f))
-
 	print ''
 	print 'to move  if not f in tnames and not dir_has_rars(f)   :'
 
-	to_move = [f for f in possibles if not f in tnames and not dir_has_rars(f)]
+	to_move = [f for f in possibles if not f in tnames and not dir_has_rars(complete_dir + "/" + f)]
 
 	for f in to_move:
 		print f
@@ -68,10 +68,10 @@ def main(complete_dir, out_dir):
 	print ''
 	print 'to move  if not f in tnames and dir_has_rars(f):'
 
-	not_to_move = [f for f in possibles if not f in tnames and dir_has_rars(f)]
+	not_to_move = [f for f in possibles if not f in tnames and dir_has_rars(complete_dir + "/" + f)]
 
-	for f in not_to_move:
-		print f + "  ** HAS RARS"
+	# for f in not_to_move:
+	# 	print f + "  ** HAS RARS"
 
 	print ''
 
@@ -79,4 +79,9 @@ def main(complete_dir, out_dir):
 	#print ''
 
 if __name__ == "__main__":
+
+	# path = complete_dir + "/The.Walking.Dead.S05E09.720p.HDTV.x264-KILLERS"
+	# print path
+	# print dir_has_rars(path)
+
 	main(complete_dir, out_dir)
