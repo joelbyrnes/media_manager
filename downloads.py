3dcdce64169d063e5b20efd798a57ebed410e4b6
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-07-27 00:07:51 +1000

    delete files if not set False

--- downloads.py
+++ downloads.py
@@ -3,12 +3,14 @@ import os
 import sys
 import csv
 import click
+import shutil
 from extract import extract, dir_has_rars
 
 def move_files(names, cwd, out_dir):
 	for f in names:
-		print "moving %s to %s/%s" % (f, out_dir, f)
-		os.rename("%s/%s" % (cwd, f), "%s/%s" % (out_dir, f))
+		dest = os.path.join(out_dir, f)
+		print "moving %s to %s" % (f, dest)
+		shutil.move(os.path.join(cwd, f), dest)
 
 def printall(candidates, title):
 	print title
@@ -67,7 +69,7 @@ def write_report(filename, candidates):
 				row.append('???')
 			output_writer.writerow(row)
 
-def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False):
+def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False, delete=True):
 
 	torrents = tc.get_torrents()
 
@@ -80,7 +82,7 @@ def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False):
 
 	def create_data(p):
 		torrent = tdict[p] if p in tdict else None
-		path = complete_dir + "/" + p
+		path = os.path.join(complete_dir, p)
 		data = {'name': p, 'path': path, 'torrent': torrent, 'is_dir': os.path.isdir(path),
 				'has_rars': dir_has_rars(path)}
 		# print data
@@ -127,22 +129,32 @@ def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False):
 	to_extract = [c for c in possibles if not c['torrent'] and c['has_rars']]
 	printall(to_extract, "no torrent but has rars, to extract and/or delete:")
 
-	for c in to_extract[0:1]:
-		extract(os.path.join(complete_dir, c['name']), extract_dir, dry_run)
-		# TODO delete
+	for c in to_extract:
+		extract(c['path'], extract_dir, dry_run)
+		if not dry_run and delete:
+			shutil.rmtree(c['path'])
+		else:
+			print "dry run only"
 
-	# to_extract_and_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and c['has_rars']]
-	# to_extract_and_remove.extend([c for c in possibles if (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and c['has_rars']])
-	# printall(to_extract_and_remove, "finished torrent with rars, to extract and remove torrent and data:")
+	to_extract_and_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and c['has_rars']]
+	to_extract_and_remove.extend([c for c in possibles if (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and c['has_rars']])
+	printall(to_extract_and_remove, "finished torrent with rars, to extract and remove torrent and data:")
 
-	# tc.remove_torrent(ids, delete_data=True)
+	for c in to_extract_and_remove:
+		extract(c['path'], extract_dir, dry_run)
+		if not dry_run and delete:
+			tc.stop_torrent(c['torrent'].id)
+			tc.remove_torrent(c['torrent'].hashString, delete_data=True)
+		else:
+			print "dry run only"
 
 @click.command()
 @click.option('--completed_dir', prompt=True, help='Root of download location')
 @click.option('--move_dir', prompt=True, help='Dir to move finished torrents to')
 @click.option('--dest', prompt=True, help='Extracted files destination')
 @click.option('--dry_run', help='Don\'t actually make changes', default=False)
-def main(completed_dir, move_dir, dest, dry_run):
+@click.option('--delete', help='Delete files', default=True)
+def main(completed_dir, move_dir, dest, dry_run, delete):
 	# TODO params
 	tc = transmissionrpc.Client('mac-mini.local', port=9091)
 
