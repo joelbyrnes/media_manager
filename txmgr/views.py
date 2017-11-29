673c1526b5489473cb053abed82156a83ecea43a
Joel Byrnes <fatal.exception.occurred@gmail.com>
2017-11-29 22:54:21 +1000

    basic wiring

--- txmgr/views.py
+++ txmgr/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    torrents = []
+    return render(request, 'txmgr/index.html', {'torrents': torrents})
