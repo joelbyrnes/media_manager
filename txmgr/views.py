9ea2b68ec60f1e960a9d11c9ea69ebed2efc9645
Joel Byrnes <fatal.exception.occurred@gmail.com>
2017-11-30 00:42:51 +1000

    add bootstrap, create TorrentView

--- txmgr/views.py
+++ txmgr/views.py
@@ -4,10 +4,39 @@ from __future__ import unicode_literals
 import logging
 
 import transmissionrpc
+from datetime import datetime
 from django.conf import settings
 from django.views import generic
 
 logger = logging.getLogger(__name__)
+
+
+def list_has_rars(files):
+    for f in files:
+        if f.lower().endswith(".rar") or f.lower().endswith(".r00"):
+            logger.debug("found rar-like file %s" % f)
+            return True
+    return False
+
+
+class TorrentView(object):
+    def __init__(self, torrent):
+        self.torrent = torrent
+
+    def __getattr__(self, key):
+        return getattr(self.torrent, key)
+
+    # def __setattr__(self, key, value):
+    #     return setattr(self.torrent, key, value)
+
+    def has_rars(self):
+        return list_has_rars([f['name'] for f in self.torrent.files().values()])
+
+    def percent_done_string(self):
+        return "{:.1%}".format(self.torrent.percentDone)
+
+    def done_date(self):
+        return datetime.fromtimestamp(self.torrent.doneDate)
 
 
 class IndexView(generic.ListView):
@@ -18,7 +47,7 @@ class IndexView(generic.ListView):
         logger.debug("Connect to Transmission")
         tc = transmissionrpc.Client(**settings.TRANSMISSION_CONFIG)
         logger.debug("get_torrents")
-        torrents = tc.get_torrents()
+        torrents = map(TorrentView, tc.get_torrents())
         logger.debug("got torrents")
 
         return sorted(torrents, key=lambda x: x.name)
