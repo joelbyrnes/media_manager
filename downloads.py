c49e6c89d72dee29fd135e6cd20ffb6ab9d37dc0
Joel Byrnes <fatal.exception.occurred@gmail.com>
2017-11-29 22:57:32 +1000

    fix pep8

--- downloads.py
+++ downloads.py
@@ -6,147 +6,155 @@ import click
 import shutil
 from extract import extract, dir_has_rars
 
+
 def move_files(names, cwd, out_dir):
-	for f in names:
-		dest = os.path.join(out_dir, f)
-		print "moving %s to %s" % (f, dest)
-		shutil.move(os.path.join(cwd, f), dest)
+    for f in names:
+        dest = os.path.join(out_dir, f)
+        print "moving %s to %s" % (f, dest)
+        shutil.move(os.path.join(cwd, f), dest)
+
 
 def printall(candidates, title):
-	print title
-	print len(candidates)
-	for c in candidates:
-		print c['name']
-	print ''
+    print title
+    print len(candidates)
+    for c in candidates:
+        print c['name']
+    print ''
+
 
 def write_report(filename, candidates):
-	with open(filename, 'wb') as f:
-		output_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
-		output_writer.writerow([
-			'name',
-			'has_torrent',
-			'date_added',
-			'status',
-			'error',
-			'errorString',
-			'isFinished',
-			'is_dir',
-			'has_rars',
-			'action',
-			])
-		for c in candidates:
-			t = c['torrent']
+    with open(filename, 'wb') as f:
+        output_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+        output_writer.writerow([
+            'name',
+            'has_torrent',
+            'date_added',
+            'status',
+            'error',
+            'errorString',
+            'isFinished',
+            'is_dir',
+            'has_rars',
+            'action',
+        ])
+        for c in candidates:
+            t = c['torrent']
 
-			row = [
-				c['name'],
-				t is not None,
-				t.date_added if t else None,
-				t.status if t else None,
-				t.error if t else None,
-				t.errorString if t else None,
-				t.isFinished if t else None,
-				c['is_dir'],
-				c['has_rars'],
-				]
+            row = [
+                c['name'],
+                t is not None,
+                t.date_added if t else None,
+                t.status if t else None,
+                t.error if t else None,
+                t.errorString if t else None,
+                t.isFinished if t else None,
+                c['is_dir'],
+                c['has_rars'],
+            ]
 
-			if t and t.error != 0:
-				print t, t.error, t.errorString
+            if t and t.error != 0:
+                print t, t.error, t.errorString
 
-			if not t and not c['has_rars']:
-				row.append('move')
-			elif not t and c['has_rars']:
-				row.append('extract and delete files')
-			elif (t and t.isFinished) and not c['has_rars']:
-				row.append('move and remove torrent')
-			elif (t and t.status == "stopped" and t.error == 0) and not c['has_rars']:
-				row.append('move and remove torrent')
-			elif (t and t.status == "stopped" and t.error == 0) and c['has_rars']:
-				row.append('extract files and remove torrent and data')
+            if not t and not c['has_rars']:
+                row.append('move')
+            elif not t and c['has_rars']:
+                row.append('extract and delete files')
+            elif (t and t.isFinished) and not c['has_rars']:
+                row.append('move and remove torrent')
+            elif (t and t.status == "stopped" and t.error == 0) and not c['has_rars']:
+                row.append('move and remove torrent')
+            elif (t and t.status == "stopped" and t.error == 0) and c['has_rars']:
+                row.append('extract files and remove torrent and data')
 
-			elif t and t.status == "seeding" or t.status == "downloading" or t.status == "download pending" or t.status == "checking":
-				row.append('-')
-			else:
-				row.append('???')
-			output_writer.writerow(row)
+            elif t and t.status == "seeding" or t.status == "downloading" or t.status == "download pending" or t.status == "checking":
+                row.append('-')
+            else:
+                row.append('???')
+            output_writer.writerow(row)
+
 
 def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False, delete=True):
+    torrents = tc.get_torrents()
 
-	torrents = tc.get_torrents()
+    tdict = {t.name: t for t in torrents}
 
-	tdict = {t.name: t for t in torrents}
+    on_disk = [x for x in os.listdir(complete_dir) if not x.startswith(".")]
 
-	on_disk = [x for x in os.listdir(complete_dir) if not x.startswith(".")]
+    print "%d paths to check" % len(on_disk)
+    print "%d torrents" % len(torrents)
 
-	print "%d paths to check" % len(on_disk)
-	print "%d torrents" % len(torrents)
+    def create_data(p):
+        torrent = tdict[p] if p in tdict else None
+        path = os.path.join(complete_dir, p)
+        data = {'name': p, 'path': path, 'torrent': torrent, 'is_dir': os.path.isdir(path),
+                'has_rars': dir_has_rars(path)}
+        # print data
+        return data
 
-	def create_data(p):
-		torrent = tdict[p] if p in tdict else None
-		path = os.path.join(complete_dir, p)
-		data = {'name': p, 'path': path, 'torrent': torrent, 'is_dir': os.path.isdir(path),
-				'has_rars': dir_has_rars(path)}
-		# print data
-		return data
+    possibles = map(create_data, on_disk)
 
-	possibles = map(create_data, on_disk)
+    write_report("report.csv", possibles)
+    print "wrote report to report.csv"
 
-	write_report("report.csv", possibles)
-	print "wrote report to report.csv"
+    print ''
 
-	print ''
+    # printall([c for c in possibles if not c['is_dir']], "single files:")
 
-	# printall([c for c in possibles if not c['is_dir']], "single files:")
+    to_move = [c for c in possibles if not c['torrent'] and not c['has_rars']]
 
-	to_move = [c for c in possibles if not c['torrent'] and not c['has_rars']]
+    to_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and not c['has_rars']]
+    to_remove.extend([c for c in possibles if
+                      (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and not c[
+                          'has_rars']])
 
-	to_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and not c['has_rars']]
-	to_remove.extend([c for c in possibles if (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and not c['has_rars']])
+    for r in to_remove:
+        print "removing torrent " + r['name']
+        if not dry_run:
+            t = r['torrent']
+            tc.stop_torrent(t.id)
+            tc.remove_torrent(t.hashString)
+            pass
+        else:
+            print "dry run only"
 
-	for r in to_remove:
-		print "removing torrent " + r['name']
-		if not dry_run:
-			t = r['torrent']
-			tc.stop_torrent(t.id)
-			tc.remove_torrent(t.hashString)
-			pass
-		else:
-			print "dry run only"
+    to_move.extend(to_remove)
 
-	to_move.extend(to_remove)
+    print ''
+    if to_move:
+        print "moving %d dirs/files" % len(to_move)
+        if not dry_run:
+            move_files([c['name'] for c in to_move], complete_dir, out_dir)
+            print ''
+        else:
+            print "dry run only"
+    else:
+        print 'nothing to move'
+    print ''
 
-	print ''
-	if to_move:
-		print "moving %d dirs/files" % len(to_move)
-		if not dry_run:
-			move_files([c['name'] for c in to_move], complete_dir, out_dir)
-			print ''
-		else:
-			print "dry run only"
-	else:
-		print 'nothing to move'
-	print ''
+    to_extract = [c for c in possibles if not c['torrent'] and c['has_rars']]
+    printall(to_extract, "no torrent but has rars, to extract and/or delete:")
 
-	to_extract = [c for c in possibles if not c['torrent'] and c['has_rars']]
-	printall(to_extract, "no torrent but has rars, to extract and/or delete:")
+    for c in to_extract:
+        extract(c['path'], extract_dir, dry_run)
+        if not dry_run and delete:
+            shutil.rmtree(c['path'])
+        else:
+            print "dry run only"
 
-	for c in to_extract:
-		extract(c['path'], extract_dir, dry_run)
-		if not dry_run and delete:
-			shutil.rmtree(c['path'])
-		else:
-			print "dry run only"
+    to_extract_and_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and c['has_rars']]
+    to_extract_and_remove.extend([c for c in possibles if
+                                  (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and c[
+                                      'has_rars']])
+    printall(to_extract_and_remove, "finished torrent with rars, to extract and remove torrent and data:")
 
-	to_extract_and_remove = [c for c in possibles if (c['torrent'] and c['torrent'].isFinished) and c['has_rars']]
-	to_extract_and_remove.extend([c for c in possibles if (c['torrent'] and c['torrent'].status == "stopped" and c['torrent'].error == 0) and c['has_rars']])
-	printall(to_extract_and_remove, "finished torrent with rars, to extract and remove torrent and data:")
+    for c in to_extract_and_remove:
+        extract(c['path'], extract_dir, dry_run)
+        if not dry_run and delete:
+            tc.stop_torrent(c['torrent'].id)
+            tc.remove_torrent(c['torrent'].hashString, delete_data=True)
+        else:
+            print "dry run only"
 
-	for c in to_extract_and_remove:
-		extract(c['path'], extract_dir, dry_run)
-		if not dry_run and delete:
-			tc.stop_torrent(c['torrent'].id)
-			tc.remove_torrent(c['torrent'].hashString, delete_data=True)
-		else:
-			print "dry run only"
 
 @click.command()
 @click.option('--completed_dir', prompt=True, help='Root of download location')
@@ -155,10 +163,11 @@ def manage(tc, complete_dir, out_dir, extract_dir, dry_run=False, delete=True):
 @click.option('--dry_run', help='Don\'t actually make changes', default=False)
 @click.option('--delete', help='Delete files', default=True)
 def main(completed_dir, move_dir, dest, dry_run, delete):
-	# TODO params
-	tc = transmissionrpc.Client('mac-mini.local', port=9091)
+    # TODO params
+    tc = transmissionrpc.Client('mac-mini.local', port=9091)
 
-	manage(tc, completed_dir, move_dir, dest, dry_run, delete)
+    manage(tc, completed_dir, move_dir, dest, dry_run, delete)
+
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
--- extract.py
+++ extract.py
@@ -5,116 +5,120 @@ from subprocess import call
 import shutil
 import click
 
+
 def find_first_rar(files):
-	def subfilter(fs):
-		for f in fs:
-			if ".subpack." in f.lower() or "/subs/" in f.lower() or "subs" in f.lower():
-				# print "(found rar-like file %s that looks like subpack, ignoring)"
-				pass
-			else:
-				# print "(found rar-like file %s)" % f
-				return f
+    def subfilter(fs):
+        for f in fs:
+            if ".subpack." in f.lower() or "/subs/" in f.lower() or "subs" in f.lower():
+                # print "(found rar-like file %s that looks like subpack, ignoring)"
+                pass
+            else:
+                # print "(found rar-like file %s)" % f
+                return f
 
-	rars = filter(lambda f: f.lower().endswith(".rar"), files)
-	rar = subfilter(rars)
-	if rar:
-		return rar
+    rars = filter(lambda f: f.lower().endswith(".rar"), files)
+    rar = subfilter(rars)
+    if rar:
+        return rar
 
-	r00s = filter(lambda f: f.lower().endswith(".r00"), files)
-	r00 = subfilter(r00s)
-	if r00:
-		return r00
+    r00s = filter(lambda f: f.lower().endswith(".r00"), files)
+    r00 = subfilter(r00s)
+    if r00:
+        return r00
 
-	return None
+    return None
+
 
 def dir_has_rars(path):
-	if not os.path.exists(path): raise Exception("path does not exist: " + path)
-	if not os.path.isdir(path): return False
+    if not os.path.exists(path): raise Exception("path does not exist: " + path)
+    if not os.path.isdir(path): return False
 
-	for root, subdirs, files in os.walk(path):
-		if find_first_rar(files) != None:
-			return True
-	return False
+    for root, subdirs, files in os.walk(path):
+        if find_first_rar(files) != None:
+            return True
+    return False
+
 
 def copy_files(names, cwd, out_dir):
-	for f in names:
-		dest = os.path.join(out_dir, f)
-		print "copying %s to %s" % (f, dest)
-		shutil.copy2(os.path.join(cwd, f), dest)
+    for f in names:
+        dest = os.path.join(out_dir, f)
+        print "copying %s to %s" % (f, dest)
+        shutil.copy2(os.path.join(cwd, f), dest)
 
 
 # figure out if we need to extract and do it
 def extract(src, extract_dir, dry_run=False):
+    src = os.path.abspath(src)
+    dirname = os.path.dirname(src)
+    basename = os.path.basename(src)
+    dest = os.path.join(extract_dir, basename)
 
-	src = os.path.abspath(src)
-	dirname = os.path.dirname(src)
-	basename = os.path.basename(src)
-	dest = os.path.join(extract_dir, basename)
+    # print path
+    if not os.path.exists(src):
+        print "path does not exist"
+        return
 
-	# print path
-	if not os.path.exists(src):
-		print "path does not exist"
-		return
+    if not dir_has_rars(src):
+        print "no rars found to extract - do recursive copy"
+        return
 
-	if not dir_has_rars(src):
-		print "no rars found to extract - do recursive copy"
-		return
+    print "Trying to extract/copy %s to %s" % (src, dest)
 
-	print "Trying to extract/copy %s to %s" % (src, dest)
+    for root, subdirs, files in os.walk(src):
+        print "-- " + root
+        relative_path = root.split(dirname + "/")[1]
+        current_dest = os.path.join(extract_dir, relative_path)
 
-	for root, subdirs, files in os.walk(src):
-		print "-- " + root
-		relative_path = root.split(dirname + "/")[1]
-		current_dest = os.path.join(extract_dir, relative_path)
+        if not os.path.exists(current_dest):
+            os.makedirs(current_dest)
 
-		if not os.path.exists(current_dest):
-			os.makedirs(current_dest)
+        rar = find_first_rar(files)
+        if rar:
+            rar_path = os.path.join(root, rar)
+            print "extract " + rar_path + " to " + current_dest
 
-		rar = find_first_rar(files)
-		if rar:
-			rar_path = os.path.join(root, rar)
-			print "extract " + rar_path + " to " + current_dest
+            if not dry_run:
+                # extract with full path, do not overwrite
+                code = call(["unrar", "x", '-o-', rar_path, current_dest])
 
-			if not dry_run:
-				# extract with full path, do not overwrite
-				code = call(["unrar", "x", '-o-', rar_path, current_dest])
+                if code != 0 and code != 10:
+                    # 10 is "No files to extract" - can mean files already exists, or can't find dest.
+                    # raise Exception("call to unrar returned code %d" % code)
+                    print "ERROR: call to unrar returned code %d" % code
+            else:
+                print "dry run only"
 
-				if code != 0 and code != 10:
-					# 10 is "No files to extract" - can mean files already exists, or can't find dest.
-					# raise Exception("call to unrar returned code %d" % code)
-					print "ERROR: call to unrar returned code %d" % code
-			else:
-				print "dry run only"
+            # copy other non-rar files
+            # print re.findall(".rar$", )
+            others = filter(lambda f: not f.endswith('rar') and not re.match(".*\.r\d\d$", f), files)
+            print "copy to " + root, others
+            if not dry_run:
+                copy_files(others, root, current_dest)
+            else:
+                print "dry run only"
 
-			# copy other non-rar files
-			# print re.findall(".rar$", )
-			others = filter(lambda f: not f.endswith('rar') and not re.match(".*\.r\d\d$", f), files)
-			print "copy to " + root, others
-			if not dry_run:
-				copy_files(others, root, current_dest)
-			else:
-				print "dry run only"
+        else:
+            if subdirs:
+                print "dir has subdirs but no rars"
+            if not subdirs:
+                print "dir has no subdirs: copy all"
+                print "copy to " + current_dest, files
+                if not dry_run:
+                    copy_files(files, root, current_dest)
+                else:
+                    print "dry run only"
 
-		else:
-			if subdirs:
-				print "dir has subdirs but no rars"
-			if not subdirs:
-				print "dir has no subdirs: copy all"
-				print "copy to " + current_dest, files
-				if not dry_run:
-					copy_files(files, root, current_dest)
-				else:
-					print "dry run only"
+    print ''
+    print "Done extracting " + basename
 
-	print ''
-	print "Done extracting " + basename
 
 @click.command()
 @click.option('--src', prompt=True, help='Folder to extract/copy')
 @click.option('--dest', prompt=True, help='Destination')
 @click.option('--dry_run', help='Don\'t actually make changes', default=False)
 def main(src, dest, dry_run):
-	extract(src, dest, dry_run)
+    extract(src, dest, dry_run)
+
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
