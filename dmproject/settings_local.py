d5332c371f277496c1024dc9fd3f9761322dd3ba
Joel Byrnes <fatal.exception.occurred@gmail.com>
2017-12-02 15:06:44 +1000

    add local settings for testing on network

---
+++ dmproject/settings_local.py
@@ -0,0 +1,5 @@
+from settings import *
+
+from socket import gethostname, gethostbyname
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', gethostname(), gethostbyname(gethostname())]
+ALLOWED_HOSTS += ['192.168.0.%s' % i for i in range(256)]
